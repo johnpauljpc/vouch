e3442b685892f4940be08d783c1b5541ee2132ae
johnpauljpc <jpcwork081@gmail.com>
2026-01-28 02:54:21 +0100

    address view logic implemented

--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,38 @@ from rest_framework.response import Response
 from rest_framework import status, generics
 from drf_spectacular.utils import extend_schema
 
-from .serializers import CheckoutSerializer, OrderSerializers, OrderItemSerializer
+from .models import Address
+from .serializers import AddrSerializer,CheckoutSerializer, OrderSerializers, OrderItemSerializer
+
+
+class AddressCreateListView(generics.ListCreateAPIView):
+    '''
+    GET  /api/addresses/      -> list user's addresses
+    POST /api/addresses/      -> create a new address for logged in user
+    '''
+
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddrSerializer
+
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user).order_by('-id')
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+
+class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    GET    /api/addresses/{id}/   -> retrieve one of my addresses
+    PATCH  /api/addresses/{id}/   -> update one of my addresses
+    DELETE /api/addresses/{id}/   -> delete one of my addresses
+    '''
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddrSerializer
+
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user)
+
+
 
 
 class CheckoutView(generics.CreateAPIView):
